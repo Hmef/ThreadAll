60b6bbae720ba1548c2c26c206f3b6493df59167
Hmef <hmeftaah@gmail.com>
2023-07-03 20:57:12 +0200

    ThreadAll - starting state ( start() )

--- src/com/ThreadAll/tutorialsPoint/CreatingRunning/RunnableDemo.java
+++ src/com/ThreadAll/tutorialsPoint/CreatingRunning/RunnableDemo.java
@@ -18,10 +18,19 @@ public class RunnableDemo implements Runnable{
 		System.out.println(" ");
 		for(int i=4; i>0; i--) {
 			System.out.println(" Thread " + threadName + " , " + i );
-			//Thread.sleep(50);
-			
+			//Thread.sleep(50);	
 		}
+		System.out.println(" ==> Thread " + threadName + " exiting. ");
 	}
 
+	public void start() {
+		System.out.println(" ");
+		System.out.println(" Starting " + threadName);
+		if(t == null) {
+			t = new Thread(this, threadName);
+			t.start();
+		}
+		
+	}
 
 }
