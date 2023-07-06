e30e227aaa2c4726351e0c24f3af6ba125013498
Hmef <hmeftaah@gmail.com>
2023-07-06 22:50:32 +0200

    Thread.interrupted - ThreadGroup

--- src/com/ThreadAll/ThreadGroup/Main.java
+++ src/com/ThreadAll/ThreadGroup/Main.java
@@ -9,6 +9,13 @@ public class Main {
 		Thread t = new Thread(myThreadGroup, runnable);
 		System.out.println(" Thread Group : " + t.getThreadGroup());
 		System.out.println(" Thread Group Name : " + t.getThreadGroup().getName());
+		System.out.println(" number of active threads in the Group : " + t.getThreadGroup().activeCount());
 		t.start();
+		System.out.println(" number of active threads in the Group : " + t.getThreadGroup().activeCount());
+		//t.notify();
+		System.out.println(" thread is alive : " + t.isAlive());
+		Thread.interrupted();
+		System.out.println(" thread is interreptued : " + t.isInterrupted()); // !!!!!
+		
 	}
 }
